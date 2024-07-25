f9122d0174b67ae5d11b01e8cfc3070f5a195df2
Cecchy2 <dariocecchinato@gmail.com>
2024-07-25 14:17:58 +0200

    esercizio 1 finito

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import entities.Dipendente;
+
+import static enums.Dipartimenti.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Dipendente dipendente1 = new Dipendente(4434552, 20000.00, PRODUZIONE);
+        Dipendente dipendente2 = new Dipendente(5663228, 40000.00, VENDITE);
+        Dipendente dipendente3 = new Dipendente(4434552, 50000.00, AMMINISTRAZIONE);
+
+
+        Dipendente[] dipendenti = {dipendente1, dipendente2, dipendente3};
+
+        for (int i = 0; i < dipendenti.length; i++) {
+            System.out.println("Le matricole dei dipendenti sono: " + dipendenti[i].getMatricola());
+
+        }
+
+
     }
+
+
 }
--- src/entities/Dipendente.java
+++ src/entities/Dipendente.java
@@ -1,4 +1,38 @@
 package entities;
 
+import enums.Dipartimenti;
+
 public class Dipendente {
+    private int matricola;
+    private double stipendio;
+    private Dipartimenti dipartimento;
+
+
+    //lista costruttori
+
+    public Dipendente(int matricola, double stipendio, Dipartimenti dipartimento) {
+        this.matricola = matricola;
+        this.stipendio = stipendio;
+        this.dipartimento = dipartimento;
+    }
+
+
+    //lista metodi
+
+
+    public int getMatricola() {
+        return matricola;
+    }
+
+    public double getStipendio() {
+        return stipendio;
+    }
+
+    public Dipartimenti getDipartimento() {
+        return dipartimento;
+    }
+
+    public void setDipartimento(Dipartimenti dipartimento) {
+        this.dipartimento = dipartimento;
+    }
 }
